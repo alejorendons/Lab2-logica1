c9e25b500ac20e669d9c3eed6ce8f597466a1575
Diego Alejandro Rendon Suaza <diegoo.alejo74@gmail.com>
2023-10-05 10:53:48 -0500

    feat: Subida de proyecto

---
+++ lab2/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnSolucion = new System.Windows.Forms.Button();
+            this.respuesta = new System.Windows.Forms.Label();
+            this.panelGrafico2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 313);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(273, 38);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 406);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(273, 37);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // btnSolucion
+            // 
+            this.btnSolucion.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnSolucion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSolucion.Location = new System.Drawing.Point(152, 498);
+            this.btnSolucion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSolucion.Name = "btnSolucion";
+            this.btnSolucion.Size = new System.Drawing.Size(104, 41);
+            this.btnSolucion.TabIndex = 2;
+            this.btnSolucion.Text = "Solucion";
+            this.btnSolucion.UseVisualStyleBackColor = false;
+            this.btnSolucion.Click += new System.EventHandler(this.btnSolucion_Click);
+            // 
+            // respuesta
+            // 
+            this.respuesta.AccessibleName = ":";
+            this.respuesta.AutoSize = true;
+            this.respuesta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.respuesta.Location = new System.Drawing.Point(479, 128);
+            this.respuesta.Name = "respuesta";
+            this.respuesta.Size = new System.Drawing.Size(14, 20);
+            this.respuesta.TabIndex = 4;
+            this.respuesta.Text = ":\r\n";
+            this.respuesta.Click += new System.EventHandler(this.respuesta_Click);
+            // 
+            // panelGrafico2
+            // 
+            this.panelGrafico2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panelGrafico2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelGrafico2.Location = new System.Drawing.Point(469, 209);
+            this.panelGrafico2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panelGrafico2.Name = "panelGrafico2";
+            this.panelGrafico2.Size = new System.Drawing.Size(379, 361);
+            this.panelGrafico2.TabIndex = 12;
+            this.panelGrafico2.Paint += new System.Windows.Forms.PaintEventHandler(this.panelGrafico2_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(72, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 25);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Ingresa la primer función:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(72, 378);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(279, 25);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Ingresa la segunda función:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Purple;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 1);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1044, 89);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Purple;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(143, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(701, 46);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Punto de corte en funciones lineales";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(45, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(307, 42);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Por favor ingrese la ecuacioón de forma \r\ncorrecta y en el orden indicado";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(115, 137);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(197, 24);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "y-y1=m(x-x1) ó y=mx+b";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Candara", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(12, 103);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(418, 23);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Las funciones deben ingresarse de la siguiente forma:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(1043, 651);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelGrafico2);
+            this.Controls.Add(this.respuesta);
+            this.Controls.Add(this.btnSolucion);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximumSize = new System.Drawing.Size(1061, 698);
+            this.MinimumSize = new System.Drawing.Size(1061, 698);
+            this.Name = "Form1";
+            this.Text = "Grafico de funciones";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label respuesta;
+        private System.Windows.Forms.Panel panelGrafico2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSolucion;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ lab2/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.panelGrafico2.Paint += new System.Windows.Forms.PaintEventHandler(this.panelGrafico2_Paint);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!EsFuncionValida(textBox1.Text))
+            {
+                textBox1.ForeColor = Color.Red;
+            }
+            else
+            {
+                textBox1.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (!EsFuncionValida(textBox2.Text))
+            {
+                textBox2.ForeColor = Color.Red;  // Cambia el color del texto a rojo si la función no es válida
+            }
+            else
+            {
+                textBox2.ForeColor = Color.Black;  // Restablece el color del texto si la función es válida
+            }
+        }       
+
+        private void btnSolucion_Click(object sender, EventArgs e)
+        {
+            panelGrafico2.Invalidate();
+            string funcion1 = textBox1.Text.Trim();
+            string funcion2 = textBox2.Text.Trim();
+
+            if (!EsFuncionValida(funcion1) || !EsFuncionValida(funcion2))
+            {
+                respuesta.Text = "Por favor, ingresa funciones válidas.";
+                return;
+            }
+
+            bool success1 = ExtraerValores(funcion1, out double m1, out double b1);
+            bool success2 = ExtraerValores(funcion2, out double m2, out double b2);
+
+            if (!success1 || !success2)
+            {
+                respuesta.Text = "Las funciones ingresadas no son válidas.";
+                return;
+            }
+
+            if (m1 == m2)
+            {
+                respuesta.Text = b1 == b2 ? "Las líneas son idénticas." : "Las líneas son paralelas.";
+            }
+            else if (m1 * m2 + 1 == 0)
+            {
+                respuesta.Text = "Las líneas son perpendiculares.";
+            }
+            else
+            {
+                double x = (b2 - b1) / (m1 - m2);
+                double y = m1 * x + b1;
+                respuesta.Text = $"Las líneas se intersectan en: ({x}, {y})";
+            }
+        }
+
+        private void respuesta_Click(object sender, EventArgs e)
+        {            
+            double m1, b1, m2, b2;
+            bool success1 = ExtraerValores(textBox1.Text, out m1, out b1);
+            bool success2 = ExtraerValores(textBox2.Text, out m2, out b2);
+
+            if (!success1 || !success2)
+            {
+                MessageBox.Show("Por favor, ingrese ecuaciones válidas.");
+                return;
+            }
+
+            
+            if (m1 == m2)
+            {
+                MessageBox.Show(b1 == b2 ? "Las rectas son idénticas." : "Las rectas son paralelas.");
+                return;
+            }
+
+            
+            double x = (b2 - b1) / (m1 - m2);
+            double y = m1 * x + b1;
+
+            
+            if (m1 * m2 == -1)
+            {
+                MessageBox.Show($"Las rectas son perpendiculares y se cruzan en el punto ({x}, {y}).");
+                return;
+            }
+
+            MessageBox.Show($"Las rectas se cruzan en el punto ({x}, {y}).");
+        }
+
+        private void panelGrafico2_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            int width = panelGrafico2.Width;
+            int height = panelGrafico2.Height;
+
+            // Establece rango y factor de escala
+            double xMin = -10;
+            double xMax = 10;
+            double yMin = -10;
+            double yMax = 10;
+
+            double xScale = width / (xMax - xMin);
+            double yScale = height / (yMax - yMin);
+
+            // Función para transformar coordenadas matemáticas a coordenadas de píxeles
+            PointF ToPixel(double x, double y)
+            {
+                return new PointF((float)((x - xMin) * xScale), (float)(height - (y - yMin) * yScale));
+            }
+
+            // Dibuja los ejes X e Y
+            g.DrawLine(Pens.Black, ToPixel(xMin, 0), ToPixel(xMax, 0)); // Eje X
+            g.DrawLine(Pens.Black, ToPixel(0, yMin), ToPixel(0, yMax)); // Eje Y
+
+            // Dibuja marcas (ticks) en los ejes
+            for (double i = xMin; i <= xMax; i++)
+            {
+                g.DrawLine(Pens.Black, ToPixel(i, -0.2), ToPixel(i, 0.2)); // Ticks para el eje X
+            }
+
+            for (double j = yMin; j <= yMax; j++)
+            {
+                g.DrawLine(Pens.Black, ToPixel(-0.2, j), ToPixel(0.2, j)); // Ticks para el eje Y
+            }
+
+            // El código para graficar las líneas de las ecuaciones
+            double m1, b1, m2, b2;
+            bool success1 = ExtraerValores(textBox1.Text, out m1, out b1);
+            bool success2 = ExtraerValores(textBox2.Text, out m2, out b2);
+
+            if (success1)
+            {
+                PointF p1 = ToPixel(xMin, m1 * xMin + b1);
+                PointF p2 = ToPixel(xMax, m1 * xMax + b1);
+                g.DrawLine(Pens.Red, p1, p2);
+            }
+
+            if (success2)
+            {
+                PointF p1 = ToPixel(xMin, m2 * xMin + b2);
+                PointF p2 = ToPixel(xMax, m2 * xMax + b2);
+                g.DrawLine(Pens.Blue, p1, p2);
+            }
+        }
+
+        private bool ExtraerValores(string ecuacion, out double m, out double b)
+        {
+            m = 0;
+            b = 0;
+            ecuacion = ecuacion.ToLower().Replace(" ", "");
+
+            
+            Match matchGeneral = Regex.Match(ecuacion, @"y=(-?\d*\.?\d*)x([+\-]\d*\.?\d*)?$");           
+            Match matchPuntoPendiente = Regex.Match(ecuacion, @"y([+\-]\d*\.?\d*)=(-?\d*\.?\d*)\(x([+\-]\d*\.?\d*)\)$");
+
+            if (matchGeneral.Success)
+            {
+                m = double.Parse(matchGeneral.Groups[1].Value);
+                b = matchGeneral.Groups[2].Success ? double.Parse(matchGeneral.Groups[2].Value) : 0;
+                return true;
+            }
+            else if (matchPuntoPendiente.Success)
+            {
+                double y1 = double.Parse(matchPuntoPendiente.Groups[1].Value);
+                m = double.Parse(matchPuntoPendiente.Groups[2].Value);
+                double x1 = double.Parse(matchPuntoPendiente.Groups[3].Value);
+                b = y1 - m * x1;
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool EsFuncionValida(string funcion)
+        {
+           funcion = funcion.Replace(" ", "");
+            string patron1 = @"^y=(-?\d*\.?\d*)x([+\-]\d*)?$";
+            string patron2 = @"^y([+\-]\d*\.?\d*)=(-?\d*\.?\d*)\(x([+\-]\d*\.?\d*)\)$";
+
+            Regex regex1 = new Regex(patron1, RegexOptions.IgnoreCase);
+            Regex regex2 = new Regex(patron2, RegexOptions.IgnoreCase);
+
+            return regex1.IsMatch(funcion) || regex2.IsMatch(funcion);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }  
+}
